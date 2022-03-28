a87bbd96c4fcadb2301117feb76376ea060d11d7
eurico77 <euricomagalhaes61@gmail.com>
2022-03-28 18:18:23 -0300

    adjustments

--- src/domain/use-cases/get-home-page.usecase.ts
+++ src/domain/use-cases/get-home-page.usecase.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { HomePageDto } from 'src/aplication/resolvers/modules/page-home/types/page-home';
-import { Pages, StrapiClientService } from 'src/infra/gateways/strapi';
+import { StrapiClientService } from 'src/infra/gateways/strapi';
+import { Pages } from 'src/infra/gateways/strapi/helpers';
 
 @Injectable()
 export class GetHomePageUseCase {
   constructor(private clientService: StrapiClientService<HomePageDto>) {}
 
-  async execute(): Promise<HomePageDto> {
-    const homePage = await this.clientService.getPage(Pages.HomePage);
-    return homePage;
+  execute(): Promise<HomePageDto> {
+    return this.clientService.getPage(Pages.HomePage);
   }
 }
